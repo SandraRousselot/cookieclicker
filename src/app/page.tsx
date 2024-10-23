b55de303c32453874eb92e4af102abf2a8564ceb
SandraRousselot <zindrarouzeleau@gmail.com>
2024-10-23 15:50:52 +0200

    Couleurs horribles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  const [cookies, setCookies] = useState<number>(0)
   return (
-    <div>
-      <p>HELLO</p>
+    <div className="h-screen w-screen flex">
+      <div className="left w-1/4 bg-pink-500"><p>coucou</p></div>
+      <div className="center flex-1 bg-purple-500"></div>
+      <div className="right w-1/4 bg-orange-500"><p>toi</p></div>
     </div>
   );
 }
